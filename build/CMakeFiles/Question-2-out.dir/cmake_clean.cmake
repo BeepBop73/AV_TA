7f0f326497710ca44c09134fd49e00bb3cdeb62c
BeepBop73 <ishasgoraya@gmail.com>
2025-03-09 16:26:40 +1100

    Completed question 1 and 2

---
+++ build/CMakeFiles/Question-2-out.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Question-2-out.dir/Question-2/Question-2.cc.obj"
+  "CMakeFiles/Question-2-out.dir/Question-2/Question-2.cc.obj.d"
+  "Question-2-out.exe"
+  "Question-2-out.exe.manifest"
+  "Question-2-out.pdb"
+  "libQuestion-2-out.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Question-2-out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
